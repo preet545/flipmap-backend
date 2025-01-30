bf617fe2a7898b0317733a1d25c2a2fa7e5151a9
Phoenix Angulo <26429095+Kajmany@users.noreply.github.com>
2025-01-29 17:23:12 -0800

    It compiles

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,100 @@
-// mod geo_json;
-use std::env;
-use std::io::{Read, Write};
-use tokio::net::TcpStream;
+use axum::{http::StatusCode, response::IntoResponse, routing::post, Json, Router};
+use geojson::Position;
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+mod consts;
+mod requester;
 
-// use geo::LineString;
-use hello_osm::map_range;
-
-// TODO phase this out and receive coordinates
-const USER_LAT: &str = "44.560975";
-const USER_LON: &str = "-123.275227";
+use crate::requester::{ExternalRequester, OpenRouteRequest, PhotonGeocodeRequest};
+// I think the struct is thread safe (reqwest client has an arc internally) but the compiler
+// is really mad if I don't use sync stuff to make this static
+// TODO: Initialize this in a less dirty/global way later on
+use axum::extract::State;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let client = Arc::new(ExternalRequester::new());
+    //const CLIENT: LazyLock<ExternalRequester> = LazyLock::new(|| ExternalRequester::new());
+    //let CLIENT: ExternalRequester = ExternalRequester::new();
+    let app: Router = Router::new()
+        .route("/route", post(route))
+        .with_state(client);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
     Ok(())
+}
+
+#[derive(Deserialize)]
+pub struct RouteRequest {
+    pub lat: f64,
+    pub lon: f64,
+    pub query: String,
+}
+
+#[derive(Serialize)]
+pub struct RouteResponse {
+    // A position vec, but concatenated so it's just vec<num...> instead of vec<vec<num..>..>
+    pub route: Vec<f64>,
+}
+
+#[axum::debug_handler]
+async fn route(
+    State(client): State<Arc<ExternalRequester>>,
+    Json(params): Json<RouteRequest>,
+) -> impl IntoResponse {
+    /*
+    // Photon will also do this (and identify the wrong param) but let's fail fast
+    // TODO: May or may not be preferable to do this during deserialization??
+    if (params.lat < -90.0 || params.lat > 90.0) || (params.lon < -180.0 && params.lon > 180.0) {
+        return (
+            StatusCode::BAD_REQUEST,
+            format!(
+                "One or both parameters out of range: lat:{}, lon:{}",
+                params.lat, params.lon
+            ),
+        );
+    }
+    */
+
+    // First request to know where to ask for the route's end waypoint
+    // TODO: Handle the MANY possible errors w.r.t req and parsing
+    let req = PhotonGeocodeRequest {
+        lat: Some(params.lat),
+        lon: Some(params.lon),
+        limit: 1,
+        query: params.query,
+    };
+    let res: reqwest::Response = client.photon(&req).send().await.unwrap();
+    let res_features = res.json::<geojson::FeatureCollection>().await.unwrap();
+    // All we want is the coordinates of the point. FeatureCollection -> Feature -> Point
+    let end_coord: Position = match &res_features.features[0].geometry.as_ref().unwrap().value {
+        geojson::Value::Point(x) => x.clone(),
+        _ => panic!("Got non-position geometry value"),
+    };
+
+    // Second request to actually get the route
+    let start_coord: Position = vec![params.lon, params.lat];
+    let req = OpenRouteRequest {
+        instructions: false,
+        coordinates: vec![start_coord, end_coord],
+    };
+    let res: reqwest::Response = client.ors(&req).send().await.unwrap();
+    let res_features = res.json::<geojson::FeatureCollection>().await.unwrap();
+
+    // I call this the 'sausage factory'
+    let resp = RouteResponse {
+        route: res_features
+            .into_iter()
+            .filter_map(|feat| feat.geometry)
+            .filter_map(|geo| match geo.value {
+                geojson::Value::Point(pt) => Some(pt),
+                _ => None,
+            })
+            .collect::<Vec<Vec<f64>>>()
+            .into_iter()
+            .flatten()
+            .collect(),
+    };
+    (StatusCode::OK, Json(resp))
 }
---
+++ src/requester.rs
@@ -0,0 +1,73 @@
+use std::env;
+use std::time::Duration;
+use serde::Serialize;
+
+use crate::consts;
+
+// TODO: Constructor for both these maybe
+#[derive(Serialize)]
+pub struct OpenRouteRequest {
+    pub coordinates: Vec<geojson::Position>,
+    pub instructions: bool,
+}
+
+#[derive(Serialize)]
+pub struct PhotonGeocodeRequest { 
+    pub limit: u8, // Probably just 1 for "where am I" and ~10 for a search
+    #[serde(rename(serialize="q"))]
+    pub query: String, // Might be possible to use str here
+    // TODO: Quick and dirty optional 'anchor' here 
+    // in the future we'll use a geojson type with proper deserialization
+    pub lat: Option<f64>,
+    pub lon: Option<f64>,
+}
+
+#[derive(Serialize)]
+pub struct PhotonRevGeocodeRequest {
+    pub lat: f64,
+    pub lon: f64,
+}
+
+#[derive(Clone)]
+pub struct ExternalRequester {
+    client: reqwest::Client,
+    open_route_service_key: String,
+    // We also use Photon (via Komoot) but it has an unauthenticated API
+}
+
+// Wrapper around Reqwest that tries to ape the same client/req-builder API
+// For use in making Open Route Service (routing only) or Photon (via Komoot) calls
+impl ExternalRequester {
+    pub fn new() -> Self {
+    ExternalRequester {
+            client: 
+                reqwest::Client::builder()
+                    .user_agent(consts::USER_AGENT)
+                    .timeout(Duration::from_secs(10))
+                    .https_only(true)
+                    .build()
+                    .expect("req client construction failed"),
+            open_route_service_key:
+                // TODO: Allow reading from a file too and other such logic
+                env::var("ORS_API_KEY")
+                    .expect("Place an Open Route Service API key in ORS_API_KEY env variable!")
+                    .to_string(),
+    }
+}
+    // TODO: Re-evaluate if this is useful here. This just makes futures and doesn't execute
+    pub fn ors(&self, req: &OpenRouteRequest) -> reqwest::RequestBuilder {
+        self.client.post("https://api.openrouteservice.org/v2/directions/driving-car/geojson")
+            .header("Content-Type", "application/json")
+            .header("Authorization", &self.open_route_service_key)
+            .json(req)
+    }
+
+    pub fn photon_reverse(&self, coord: PhotonRevGeocodeRequest) -> reqwest::RequestBuilder {
+        let q = [("lon",coord.lon),("lat",coord.lat)];
+        self.client.get("https://photon.komoot.io/reverse").query(&q)
+    }
+
+    pub fn photon(&self, req: &PhotonGeocodeRequest) -> reqwest::RequestBuilder {
+        self.client.get("https://photon.komoot.io/api/").query(req)
+    }
+}
